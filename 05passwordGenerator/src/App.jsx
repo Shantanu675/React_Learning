7702295b823dfc9b8eb823a9096baa5b0413eed5
Shantanu675 <shantanudanej05@gmail.com>
2025-04-02 01:06:43 +0530

    add config.js

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -27,7 +27,7 @@ function App() {
 
   }, [length, numberAllowed, symbolAllowed, setPassword])
 
-  const copyPasswordToClipboard = useCallback( () => {
+  const copyPasswordToClipboard = useCallback(() => {
     passwordRef.current?.select()
     //passwordRef.current?.setSelectionRange(0,3)
     window.navigator.clipboard.writeText(password)
