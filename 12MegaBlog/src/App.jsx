3f2410470b8e12bafb028ba729c13835f0d8d37d
Shantanu675 <shantanudanej05@gmail.com>
2025-04-02 20:26:00 +0530

    created header and footer also added redux toolkit

--- 12MegaBlog/src/App.jsx
+++ 12MegaBlog/src/App.jsx
@@ -1,15 +1,43 @@
 import process from 'process';
 import './App.css'
+import { useDispatch } from 'react-redux'
+import { useEffect, useState } from 'react';
+import authService from './appwrite/auth'
+import { login, logout } from './store/authSlice'
+import { Header } from './components'
+import { Footer } from './components'
+import { Outlet } from 'react-router-dom';
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch()
   
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if(userData){
+        dispatch(login({userData}))
+      }
+      else{
+        dispatch(logout())
+      }
+    })
+    .finally(() => {
+      setLoading(false)
+    })
+  }, [])
 
-  return (
-    <>
-      <h1>A blog app with appwrite</h1>
-    </>
-  )
+  return !loading ? (
+    <div className='min-h-screen w-full flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header/>
+        <main>
+          <Outlet/>
+        </main>
+        <Footer/>
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
--- 12MegaBlog/src/appwrite/auth.js
+++ 12MegaBlog/src/appwrite/auth.js
@@ -1,4 +1,4 @@
-import conf from '../conf.js'
+import conf from '../conf/conf.js'
 import { Client, Account } from "appwrite";
 
 export class AuthService{
